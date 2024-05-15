c998810a07524d122d0ff320d561c7b69ac5de7f
gopivasanthi <gopi.sivanappanvasanthi@emea.nttdata.com>
2024-05-15 04:47:38 +0100

    Banner component changes

--- CTSTRNSitecore/src/Feature/BasicContent/website/Controllers/BannerController.cs
+++ CTSTRNSitecore/src/Feature/BasicContent/website/Controllers/BannerController.cs
@@ -1,4 +1,6 @@
-﻿using Sitecore.Mvc.Presentation;
+﻿using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
 using Sitecore.Web.UI.WebControls;
 using System;
 using System.Collections.Generic;
@@ -6,6 +8,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TRN.Feature.BasicContent.Models;
+using TRN.Foundation.Navigation.Models;
 
 namespace TRN.Feature.BasicContent.Controllers
 {
@@ -15,11 +18,23 @@ namespace TRN.Feature.BasicContent.Controllers
         public ActionResult GetBannerDetails()
         {
             var renderingItem = RenderingContext.Current.Rendering.Item;
+            LinkField spoc = renderingItem.Fields["EventSpoc"];
+            LinkField details = renderingItem.Fields["EventDetails"];
             Banner banner = new Banner()
             {
                 BannerTitle = new HtmlString(FieldRenderer.Render(renderingItem, "BannerTitle")),
                 BannerDesc = new HtmlString(FieldRenderer.Render(renderingItem, "BannerDesc")),
                 BannerImage = new HtmlString(FieldRenderer.Render(renderingItem, "BannerImage")),
+                Spoc = new NavigationItem
+                {
+                    NavgiationItemUrl = spoc.IsInternal ? LinkManager.GetItemUrl(spoc.TargetItem) : spoc.Url,
+                    NavigationItemName = spoc.IsInternal ? spoc.TargetItem.DisplayName : spoc.Text
+                },
+                Details = new NavigationItem
+                {
+                    NavgiationItemUrl = details.IsInternal ? LinkManager.GetItemUrl(details.TargetItem) : details.Url,
+                    NavigationItemName = details.IsInternal ? details.TargetItem.DisplayName : details.Text
+                }
             };
             return View("/Views/TRN/BasicContent/Banner.cshtml", banner);
         }
--- CTSTRNSitecore/src/Feature/BasicContent/website/Models/Banner.cs
+++ CTSTRNSitecore/src/Feature/BasicContent/website/Models/Banner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TRN.Foundation.Navigation.Models;
 
 namespace TRN.Feature.BasicContent.Models
 {
@@ -10,5 +11,7 @@ namespace TRN.Feature.BasicContent.Models
         public HtmlString BannerTitle { get; set; }
         public HtmlString BannerImage { get; set; }
         public HtmlString BannerDesc { get; set; }
+        public NavigationItem Spoc { get; set; }
+        public NavigationItem Details { get; set; }
     }
 }
